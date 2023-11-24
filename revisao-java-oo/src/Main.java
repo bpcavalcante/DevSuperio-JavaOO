88368a49bd51aedaf9105acc95a408eb6789682d
Bruno Profeta Cavalcante <brunoprofetacavalcante@gmail.com>
2023-11-23 21:14:59 -0300

    Exercicio calculo do terreno

--- revisao-java-oo/src/Main.java
+++ revisao-java-oo/src/Main.java
@@ -1,33 +1,29 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        String product1 = "Computer";
-        String product2 = "Office desk";
-
-        int age = 30;
-        int code = 5290;
-        char gender = 'F';
-
-        double price1 = 2100.0;
-        double price2 = 650.50;
-        double measure = 53.234567;
-
-        System.out.println("Products:");
-        System.out.printf(product1 + ", which price is $ %.2f%n", price1);
-        System.out.printf(product2 + ", which price is $ %.2f%n", price2);
-        System.out.println();
-
-        System.out.printf("Record: " + age + " years old, code %d and gender: %s %n",code,gender);
-        System.out.println();
-
-        System.out.printf("Measue with eight decimal places: %f %n" , measure);
-        System.out.printf("Rouded (three decimal places): %.3f %n", measure);
         Locale.setDefault(Locale.US);
-        System.out.printf("US decimal point: %.3f", measure);
+        Scanner sc = new Scanner(System.in);
+
+
+        double largura,comprimento,metroQuadrado;
+
+        System.out.println("Digite a largura do terreno: ");
+        largura = sc.nextDouble();
+        System.out.println("Digite o comprimento do terreno: ");
+        comprimento = sc.nextDouble();
+        System.out.println("Digite o valor do metro quadrado: ");
+        metroQuadrado = sc.nextDouble();
+
+        double areaDoTerreno = comprimento * largura;
+        double precoDoTerreno = metroQuadrado * areaDoTerreno;
+
+        System.out.println("Area do terreno = " + areaDoTerreno);
+        System.out.println("Preco do terreno = " + precoDoTerreno);
 
     }
 }
